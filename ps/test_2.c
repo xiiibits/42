41246b703135b1444d692fed2556ffeaed6c7193
Ahmd Fahs <81890799+xiiibits@users.noreply.github.com>
2025-06-22 23:41:34 +0300

    test push swap

--- ps/test_2.c
+++ ps/test_2.c
@@ -1,41 +1,26 @@
-static void push_chunks(t_stack **stack_a, t_stack **stack_b, int chunk_size)
+void	sort_three(t_stack **a)
 {
-    int pushed;
-    int chunk_max;
-    int rotations;
-    int size_a;
+	int	first;
+	int	second;
+	int	third;
 
-    pushed = 0;
-    chunk_max = chunk_size;
-    while (stack_size(*stack_a) > 3)
-    {
-        rotations = 0;
-        size_a = stack_size(*stack_a);
-        if ((*stack_a)->index <= chunk_max)
-        {
-            pb(stack_a, stack_b);
-            pushed++;
-            if (stack_size(*stack_b) > 1 && (*stack_b)->index < chunk_max - chunk_size / 2)
-                rb(stack_b);
-        }
-        else
-        {
-            ra(stack_a);
-            rotations++;
-        }
-        
-        // If we've rotated through the entire stack without finding a number to push
-        if (rotations >= size_a)
-        {
-            chunk_max += chunk_size;
-            pushed = 0;
-            rotations = 0;
-        }
-        
-        if (pushed == chunk_size)
-        {
-            chunk_max += chunk_size;
-            pushed = 0;
-        }
-    }
+	first = (*a)->value;
+	second = (*a)->next->value;
+	third = (*a)->next->next->value;
+	if (first > second && second < third && first < third)
+		sa(a);
+	else if (first > second && second > third)
+	{
+		sa(a);
+		rra(a);
+	}
+	else if (first > second && second < third && first > third)
+		ra(a);
+	else if (first < second && second > third && first < third)
+	{
+		sa(a);
+		ra(a);
+	}
+	else if (first < second && second > third && first > third)
+		rra(a);
 }
